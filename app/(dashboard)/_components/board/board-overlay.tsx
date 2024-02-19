40760d829997c1c341a17e8e4e780dd834f84e66
zerojin <qser155@naver.com>
2024-02-19 13:39:47 +0900

    feat: rectangle && ellipse components

--- app/(dashboard)/_components/board/board-overlay.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const BoardOverlay = () => {
-    return (
-        <div className="opacity-0 group-hover:opacity-50 transition-opacity h-full w-full bg-black" />
-    );
-};
