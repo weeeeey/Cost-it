cce5158190e52de876c38c8cf421cd9539c0e58c
zerojin <qser155@naver.com>
2024-02-20 17:25:00 +0900

    fix: main page searchParams - because vercel get searchParmasProps for Proxy Object

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,6 +2,7 @@
 import { useOrganization } from '@clerk/clerk-react';
 import { EmptyOrg } from './_components/empty/empty-org';
 import { BoardList } from './_components/board/board-list';
+import { useSearchParams } from 'next/navigation';
 
 interface DashboardProps {
     searchParams: {
@@ -10,9 +11,14 @@ interface DashboardProps {
     };
 }
 
-const DashboardPage = ({ searchParams }: DashboardProps) => {
+const DashboardPage = () => {
     const { organization } = useOrganization();
-    console.log(searchParams);
+
+    const search = useSearchParams();
+    const searchParams = {
+        search: search.get('search') || undefined,
+        favorites: search.get('favorites') || undefined,
+    };
     return (
         <div className=" flex-1 h-[calc(100%-80px)]">
             {!organization ? (
